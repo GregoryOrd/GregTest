a958d7206332d5624661c3928b9db6ebd6e78300
gregoryord <g.ord@hotmail.ca>
2021-02-07 11:45:01 -0700

    Added extern C section to header

--- sw/GregTest.h
+++ sw/GregTest.h
@@ -5,6 +5,14 @@
 
 #define G_ASSERT_STR_EQ(expected, actual) G_ASSERT_EQ_STR(expected, actual, __func__)
 
+#ifdef __cplusplus
+extern "C" {
+#endif  
+
 DllImport void G_ASSERT_EQ_STR(const char* expected, const char* actual, const char* testName);
+
+#ifdef __cplusplus
+}
+#endif  
 
 #endif
