06ed7c4f79a50f156f101a5ba5a4246e929e8246
gregoryord <g.ord@hotmail.ca>
2021-03-23 06:21:29 -0600

    Removing the defines in G_ASSERT.h. We don't need them in this header. Just need them in the customer facing GregTest.h

--- sw/G_ASSERT.h
+++ sw/G_ASSERT.h
@@ -1,11 +1,6 @@
 #ifndef G_ASSERT_H
 #define G_ASSERT_H
 
-#define G_ASSERT_STR_EQ(expected, actual) G_ASSERT_EQ_STR(expected, actual, __func__)
-#define G_ASSERT_INT_EQ(expected, actual) G_ASSERT_EQ_INT(expected, actual, __func__)
-#define G_ASSERT_CHAR_EQ(expected, actual) G_ASSERT_EQ_CHAR(expected, actual, __func__)
-#define G_ASSERT_UNSIGNED_CHAR_EQ(expected, actual) G_ASSERT_EQ_UNSIGNED_CHAR(expected, actual, __func__)
-
 void G_ASSERT_EQ_STR(const char* expected, const char* actual, const char* testName);
 void G_ASSERT_EQ_INT(const int expected, const int actual, const char* testName);
 void G_ASSERT_EQ_CHAR(const char expected, const char actual, const char* testName);
